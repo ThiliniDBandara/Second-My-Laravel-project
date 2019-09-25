99af70d75fce11d357ab7bc783162df9af2b00b5
thilinitd <thilini.td@gmail.com>
2019-09-25 10:55:20 +0530

    My Second Laravel Project

---
+++ Restuarant/resources/views/index.blade.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-colorpicker/2.5.3/css/bootstrap-colorpicker.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css"> 
+    <link rel="stylesheet" type="text/css" href="custom.css">
+    <title></title>
+  </head>
+  <body>
+   
+    <nav class="navbar navbar-expand-lg bg-dark nav-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a href="#page-top" class="navbar-brand">HOME</a>
+            <button type="button" class="navbar-toggler navbar-toggler-right text-dark" data-toggle="collapse" data-target="#ex1"><span class="sr-only">Toggle Navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse justify-content-end" id="ex1">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="#about">ABOUT&nbsp;&nbsp;</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#service">SERVICES&nbsp;&nbsp;</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#menu">MENU&nbsp;&nbsp;</a></li>                  
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section id="page-top" class="bg-dark mt-5">
+      <div class="container">
+        <img src="img/portfolio-3.jpg" class="img-fluid h-100 w-100">
+      </div>
+    </div>
+    </section>
+
+    <section class="border-bottom-none" id="about">
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-lg-6 col-md-12">
+            <img src="img/application-screenshot-img-06.png" class="img-fluid h-75">
+          </div>
+          <div class="col-lg-6 col-md-12 mt-5"><br><br>
+            <div class="header" style="color: darkgoldenrod">
+            <h2>The best food in town at one of the best locations!!!</h2></div>
+            <h4 class="text-muted lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries.</h4><br>
+            <p class="text-muted">Lorem Ipsum has been the industry's standard dummy text ever since the when an unknown printer took a galley of type scrambled it to make a type specimen book. Lorem Ipsum has been the industry's standard dummy text ever since the when an unknown printer took a galley of type scrambled.</p><br>
+          <a href="#menu" class="btn btn-dark btn-md rounded" >EXPLORE MENU</a>
+          </div>
+        </div>
+    </section>
+    <section class="bg-white" id="service">
+      <div class="container">
+        <div class="header text-center">
+          <h2 class="mb-4">CAFE SERVICES</h2></div>
+          <p class="text-muted text-center mb-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the <br>
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type <br>
+            and scrambled it to make a type specimen book.</p>
+        <div class="row justify-content-center align-items-center">
+          <div class="col-lg-3 col-md-6 col-sm-12">
+            <img src="img/restaurant-service-icon01.png" class="img-fluid">
+            <div class="text">
+              <h6 class="mt-4">BREAKFAST</h6>
+              <p class="text-muted mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12">
+              <img src="img/restaurant-service-icon02.png" class="img-fluid">
+              <div class="text">
+                <h6 class="mt-4">CUSTOM ORDER</h6>
+                <p class="text-muted mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</p>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <img src="img/restaurant-service-icon03.png" class="img-fluid">
+                <div class="text">
+                  <h6 class="mt-4">PARTY ORDER</h6>
+                  <p class="text-muted mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</p>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6 col-sm-12">
+                  <img src="img/restaurant-service-icon04.png" class="img-fluid">
+                  <div class="text">
+                    <h6 class="mt-4">DRINKST</h6>
+                    <p class="text-muted mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</p>
+                  </div>
+                </div>
+        </div>
+      </div>
+      <div class="container bg-cream mt-5">
+        <div class="row">
+          <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12 text-center">
+            <img src="img/restaurant-about-icon.png" class="img-fluid">
+           <h5 class="mt-4" style="color: #3e2723">Specialty of the Restaurant</h5>
+              <h1 style="color: #3e2723">DISCOVER OUR<br>
+              TASTY MEALS</h1>
+              <p class="text-muted mt-3">Lorem Ipsum is simply dummy text of the <br>
+                typesetting industry. Lorem Ipsum has been the <br>
+                industry's standard dummy text ever since the <br>
+                1500s, when an unknown printer took a galley <br>
+                of type and scrambled it to make a type <br>
+                specimen book.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="menu">
+      <div class="container">
+        <div class="header text-center">
+          <h1 class="mt-5">MAIN MENU</h1>
+        </div>
+        <p class="text-center mb-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the <br>
+          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
+      </div>
+    </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
